85fb567974721aeb0c8668892e9d4eb79081e098
PalSec <palomm@stonline.sk>
2020-04-18 07:45:04 +0200

    g

--- zbl_label.py
+++ zbl_label.py
@@ -9,16 +9,17 @@ import simple_zpl2
 import socket
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(15,GPIO.IN)
-test_input = GPIO.input(15)
+
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "192.168.0.115"
 port = 9100
 
 while True:
+   GPIO.setmode(GPIO.BOARD)
+   # GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+   GPIO.setup(15, GPIO.IN)
+   test_input = GPIO.input(15)
    if keyboard.is_pressed('q'):
      print("Koniec programu")
      break
@@ -75,5 +76,5 @@ while True:
         except:
             print("Error with the connection")
 
-else:
-  print("Vstup je 0")
+   else:
+        print("Vstup je 0")
